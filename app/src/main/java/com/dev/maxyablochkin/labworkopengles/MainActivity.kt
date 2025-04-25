b2b995ba4debe176a5f7131b39f1224fcf2ae05c
Max Yablockhin <maxgitworkcreate@gmail.com>
2025-04-25 11:28:22 +0300

    Add DisposableEffect to manage OpenGL ES resources

--- app/src/main/java/com/dev/maxyablochkin/labworkopengles/MainActivity.kt
+++ app/src/main/java/com/dev/maxyablochkin/labworkopengles/MainActivity.kt
@@ -6,10 +6,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.viewinterop.AndroidView
 import com.dev.maxyablochkin.labworkopengles.ui.theme.LabWorkOpenGLESTheme
 
@@ -31,10 +33,16 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TetrahedronFigure(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    val glSurfaceView = remember { MyGLSurfaceView(context) }
+
+    DisposableEffect(Unit) {
+        glSurfaceView.onResume()
+        onDispose { glSurfaceView.onPause() }
+    }
+
     AndroidView(
         modifier = modifier,
-        factory = { context ->
-            MyGLSurfaceView(context)
-        }
+        factory = { glSurfaceView }
     )
 }
